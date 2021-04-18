ba29968cd7c0aa863d5746fd26e03a4f7592d4c1
Nebin Mathew John <nebinnmj@gmail.com>
2021-04-18 20:28:12 +0530

    added more comments for better understanding of the code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //selectors
-const container = document.getElementById("container");
-const dynamic = document.getElementById("dynamic");
-const hollywood = document.getElementById("hollywood");
+const container = document.getElementById("container"); //div holding all the elements
+const dynamic = document.getElementById("dynamic"); //div containing hollywood and guess
+const hollywood = document.getElementById("hollywood"); //h1 tag where HOLLYWOOD is displayed
+const guess = document.getElementById("guess"); //field where name to be guessd is displayed
+const form = document.getElementById("form"); //contains !!sure!! button and input field
+const startBtn = document.getElementById("start"); //start button
 const enter = document.getElementById("enter"); //enter is the form input Field
-const guess = document.getElementById("guess");
-const button = document.getElementById("button");
-const startBtn = document.getElementById("start");
-const form = document.getElementById("form");
-const resetBtn = document.getElementById("reset");
+const button = document.getElementById("button"); //!!sure!! button
+const resetBtn = document.getElementById("reset"); //reset button
 
-//random assignments
+//initial assignments
 let containsUnderscore = false;
 let modQnPlain = "";
 const strikeStart = "<s style='color:red'>";
@@ -19,17 +19,15 @@ hollywood.innerHTML = holly;
 let count = 9;
 let names = [
   "mission impossible",
-  "wanda vision",
   "avengers",
-  "falcon and the winter soilder",
   "dr strange",
   "thor the dark world",
-  "thor ragnarock",
+  "thor ragnarok",
   "ant man",
-  "captian america the first avenger",
+  "captain america the first avenger",
   "iron man",
-  "captaian america the winter soildier",
-  "captaian america civil war",
+  "captain america the winter soldier",
+  "captain america civil war",
 ];
 let qn;
 let modQn;
@@ -40,12 +38,15 @@ startBtn.addEventListener("click", fun2);
 resetBtn.addEventListener("click", fun3);
 
 //functions
+
+//event listener for button
 function fun() {
   if (enter.value != "") {
     play();
   }
 }
 
+//event listener for startBtn
 function fun2() {
   startBtn.className = "hidden";
   guess.className = "";
@@ -55,6 +56,7 @@ function fun2() {
   guess.innerHTML = displayQn(modQn);
 }
 
+//event listener for resetBtn
 function fun3() {
   resetBtn.className = "hidden";
   form.className = "form";
@@ -66,6 +68,8 @@ function fun3() {
   guess.innerHTML = displayQn(modQn);
 }
 
+//function fun() invokes this function and checks if entered letter is in the name to be guessed or
+//if entered name is same as the name to be guessed or not and perform appropriate operation
 function play() {
   guess.innerHTML = displayQn(modQn);
   if (qn == enter.value.toLowerCase()) {
@@ -113,6 +117,7 @@ function play() {
   enter.value = "";
 }
 
+//recives a string and returns a string whose every character is replaced by '_'
 function makeQn(qna) {
   let newQn = "";
   for (i = 0; i < qna.length; i++) {
@@ -121,6 +126,7 @@ function makeQn(qna) {
   return newQn;
 }
 
+//function which replaces '_' of modiQn in all the positons where qna contains val
 function modifyQn(qna, modiQn, val) {
   if (qna.includes(val)) {
     for (i = 0; i < qna.length; i++) {
@@ -133,6 +139,7 @@ function modifyQn(qna, modiQn, val) {
   return modiQn;
 }
 
+//returns a string which contains every character of the recived string saperated by a space(' ')
 function displayQn(qna) {
   let retQna = "";
   for (i = 0; i < qna.length; i++) {
